e5bdff404a17251926b248f10514217fe4fc5673
YFun <awa@outlook.ie>
2022-03-12 15:32:27 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,5 +2,7 @@
 
 import os
 os.system("curl -sL https://git.io/file-transfer | sh ")
-a = os.popen("./transfer cat Win.zip")
+a = os.popen("./transfer wss Win.zip")
+print(a.read())
+
 print(a.read().split("\n")[1])
