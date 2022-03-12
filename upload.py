3e230da1ffe2d958ca6b73758b2cc918989c114e
YFun <awa@outlook.ie>
2022-03-12 15:33:40 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,7 +2,7 @@
 
 import os
 os.system("curl -sL https://git.io/file-transfer | sh ")
-a = os.popen("./transfer wss Win.zip")
+a = os.popen("./transfer lit Win.zip")
 print(a.read())
 
 print(a.read().split("\n")[1])
