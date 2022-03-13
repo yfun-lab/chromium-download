8004e921bb2d2b03087196ca9e31a3f9016a3115
YFun <awa@outlook.ie>
2022-03-13 11:08:50 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,27 +4,27 @@ import os
 os.system("curl -sL https://git.io/file-transfer | sh ")
 
 # LitterBox
-lit = {}
-lit["win"] = os.popen("./transfer lit Win.zip --no-progress").read()
-lit["win_x64"] = os.popen("./transfer lit Win_x64.zip --no-progress").read()
-lit["mac"] = os.popen("./transfer lit Mac.zip --no-progress").read()
-lit["mac_arm"] = os.popen("./transfer lit Mac_Arm.zip --no-progress").read()
-lit["linux"] = os.popen("./transfer lit Linux.zip --no-progress").read()
-lit["linux_x64"] = os.popen("./transfer lit Linux_x64.zip --no-progress").read()
-lit["chromiumos"] = os.popen("./transfer lit Linux_ChromiumOS_Full.zip --no-progress").read()
-lit["android"] = os.popen("./transfer lit Android.zip --no-progress").read()
+# lit = {}
+# lit["win"] = os.popen("./transfer lit Win.zip --no-progress").read()
+# lit["win_x64"] = os.popen("./transfer lit Win_x64.zip --no-progress").read()
+# lit["mac"] = os.popen("./transfer lit Mac.zip --no-progress").read()
+# lit["mac_arm"] = os.popen("./transfer lit Mac_Arm.zip --no-progress").read()
+# lit["linux"] = os.popen("./transfer lit Linux.zip --no-progress").read()
+# lit["linux_x64"] = os.popen("./transfer lit Linux_x64.zip --no-progress").read()
+# lit["chromiumos"] = os.popen("./transfer lit Linux_ChromiumOS_Full.zip --no-progress").read()
+# lit["android"] = os.popen("./transfer lit Android.zip --no-progress").read()
 
 
 # CatBox
-cat = {}
-cat["win"] = os.popen("./transfer cat Win.zip --no-progress").read()
-cat["win_x64"] = os.popen("./transfer cat Win_x64.zip --no-progress").read()
-cat["mac"] = os.popen("./transfer cat Mac.zip --no-progress").read()
-cat["mac_arm"] = os.popen("./transfer cat Mac_Arm.zip --no-progress").read()
-cat["linux"] = os.popen("./transfer cat Linux.zip --no-progress").read()
-cat["linux_x64"] = os.popen("./transfer cat Linux_x64.zip --no-progress").read()
-cat["chromiumos"] = os.popen("./transfer cat Linux_ChromiumOS_Full.zip --no-progress").read()
-cat["android"] = os.popen("./transfer cat Android.zip --no-progress").read()
+# cat = {}
+# cat["win"] = os.popen("./transfer cat Win.zip --no-progress").read()
+# cat["win_x64"] = os.popen("./transfer cat Win_x64.zip --no-progress").read()
+# cat["mac"] = os.popen("./transfer cat Mac.zip --no-progress").read()
+# cat["mac_arm"] = os.popen("./transfer cat Mac_Arm.zip --no-progress").read()
+# cat["linux"] = os.popen("./transfer cat Linux.zip --no-progress").read()
+# cat["linux_x64"] = os.popen("./transfer cat Linux_x64.zip --no-progress").read()
+# cat["chromiumos"] = os.popen("./transfer cat Linux_ChromiumOS_Full.zip --no-progress").read()
+# cat["android"] = os.popen("./transfer cat Android.zip --no-progress").read()
 
 # Null
 # null = {}
@@ -51,10 +51,10 @@ trs["android"] = os.popen("./transfer trs Android.zip --no-progress").read()
 
 with open('./README_TEMPLATE.md', 'r') as f:
     markdown = f.read()
-    for k, v in lit.items():
-        markdown.replace("@{lit_"+k+"}", v)
-    for k, v in cat.items():
-        markdown.replace("@{cat_"+k+"}", v)
+#     for k, v in lit.items():
+#         markdown.replace("@{lit_"+k+"}", v)
+#     for k, v in cat.items():
+#         markdown.replace("@{cat_"+k+"}", v)
     for k, v in trs.items():
         markdown.replace("@{trs_"+k+"}", v)
 
